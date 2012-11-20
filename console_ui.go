0efa4a4a0c780757ab2e2e80a69cf9c60d072ff4
Jeremy Neander <subjectego@gmail.com>
2012-11-20 14:48:20 -0600

    Move ReadInput() to console_ui library
    ReadInput() is not a test helper function.

--- console_ui.go
+++ console_ui.go
@@ -11,3 +11,9 @@ func ( c Console ) PromptMainMenu() {
                      "Press any key to exit... " )
   for keys := ""; len(keys) == 0; keys = ReadInput( c.in ) {}
 }
+
+func ReadInput( buffer Reader ) ( result string ) {
+  var read = make( []byte, 128 )
+  num, _ := buffer.Read( read )
+  return string( read[:num] )
+}
--- test_helper.go
+++ test_helper.go
@@ -1,7 +1,2 @@
 package tictactoe
 
-func ReadInput( buffer Reader ) ( result string ) {
-  var read = make( []byte, 128 )
-  num, _ := buffer.Read( read )
-  return string( read[:num] )
-}
