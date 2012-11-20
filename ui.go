165700d0f35b5744bdc02768e388fd7ff5ce950f
Jeremy Neander <subjectego@gmail.com>
2012-11-20 14:39:51 -0600

    Add Reader and Writer interfaces
    Methods WriteString and Read are expected by output and input,
    respectively. They should be declared on interfaces for adaptability.

--- ui.go
+++ ui.go
@@ -3,3 +3,11 @@ package tictactoe
 type UI interface {
   PromptMainMenu()
 }
+
+type Reader interface {
+  Read(b []byte) (n int, err error)
+}
+
+type Writer interface {
+  WriteString(s string) (ret int, err error)
+}
