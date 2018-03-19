9085e9aff9a4e09bf60a86519ac2c5da0118d205
Sanket Patel <spatel@egen.solutions>
2018-03-19 01:54:29 -0500

    Fix import and update README

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
-import "tic-tac-toe-go/ttt"
-import "os"
+import (
+	"os"
+
+	"github.com/jneander/tic-tac-toe-go/ttt"
+)
 
 func main() {
-  var ui = ttt.NewConsole( os.Stdin, os.Stdout )
-  var game = ttt.NewGame()
-  var p1 = ttt.NewConsolePlayer( ui )
-  var p2 = ttt.NewImpossibleComputer()
-  var players = []ttt.Player{ p1, p2 }
-  p1.SetMark( "X" )
-  p2.SetMark( "O" )
+	var ui = ttt.NewConsole(os.Stdin, os.Stdout)
+	var game = ttt.NewGame()
+	var p1 = ttt.NewConsolePlayer(ui)
+	var p2 = ttt.NewImpossibleComputer()
+	var players = []ttt.Player{p1, p2}
+	p1.SetMark("X")
+	p2.SetMark("O")
 
-  runner := ttt.ConsoleRunner{ game, ui, players }
-  runner.Run()
+	runner := ttt.ConsoleRunner{game, ui, players}
+	runner.Run()
 }
